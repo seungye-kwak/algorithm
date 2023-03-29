476bcc7b204988af529ca2bd805e79d42e5278cc
sseung_ye <112370282+seungye-kwak@users.noreply.github.com>
2023-03-29 16:24:05 +0900

    [Silver IV] Title: 큐, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+queue=[]
+for _ in range(n) :
+    msg = list(input().split())
+    
+    if msg[0] == 'push' :
+        queue.append(msg[1])
+    elif msg[0] == 'pop' :
+        if queue :
+            print(queue.pop(0))
+        else :
+            print(-1)
+    elif msg[0] == 'size':
+        print(len(queue))
+    elif msg[0] == 'empty':
+        if queue :
+            print(0)
+        else :
+            print(1)
+    elif msg[0] == 'front' :
+        if queue :
+            print(queue[0])
+        else :
+            print(-1)
+    else :
+        if queue :
+            print(queue[-1])
+        else :
+            print(-1)
