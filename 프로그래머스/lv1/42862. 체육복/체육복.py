fc2d54019553d5c19b3fb0a888d5fac2f694e638
sseung_ye <112370282+seungye-kwak@users.noreply.github.com>
2023-08-27 10:57:12 +0900

    [level 1] Title: 체육복, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,6 +1,8 @@
 def solution(n, lost, reserve):
-    reserve_set = set(reserve) - set(lost)
-    lost_set = set(lost) - set(reserve)
+    reserve_set = set(reserve) - set(lost) # 이 부분을 set(reserve) 
+    lost_set = set(lost) - set(reserve) # 이 부분을 set(lost)로 했을 때는 실패
+
+		# 자료형의 문제는 아닌 것 같고 중복되지 않는다는 조건을 코드로 구현해야 하는 듯
     for i in reserve_set:
         if i-1 in lost_set:
             lost_set.remove(i-1)
