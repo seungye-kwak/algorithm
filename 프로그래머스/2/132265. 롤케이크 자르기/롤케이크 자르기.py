2956e8b82200d883d566468f1321ff3e191e8869
sseung_ye <112370282+seungye-kwak@users.noreply.github.com>
2023-12-16 18:28:48 +0900

    [level 2] Title: 롤케이크 자르기, Time: 554.09 ms, Memory: 51.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -0,0 +1,18 @@
+from collections import Counter
+ 
+def solution(topping):
+    answer = 0
+    chulsu = Counter(topping) 
+    brother = set()
+ 
+    for t in topping:
+        chulsu[t] -= 1
+        brother.add(t)
+ 
+        if chulsu[t] == 0:
+            chulsu.pop(t)
+            
+        if len(chulsu) == len(brother):
+            answer += 1
+            
+    return answer
