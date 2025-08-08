d7f6376c2d17cf3c40f0c520ddfda00f91c0917b
sseung_ye <112370282+seungye-kwak@users.noreply.github.com>
2025-08-08 17:42:04 +0900

    [Silver I] Title: 그림, Time: 320 ms, Memory: 40032 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.py
+++ 백준/Silver/1926. 그림/그림.py
@@ -1,43 +1,41 @@
+n, m = map(int, input().split())
+paper = []
+for _ in range(n) :
+    paper.append(list(map(int, input().split())))
+
+visited = [[False] * m for _ in range(n)]
+
 from collections import deque
 
-n, m = map(int, input().split())
-papers = [[int(x) for x in input().split()] for _ in range(n)]
-
-def bfs(start, graph, visited):
-    size = 1
-    queue = deque([start])
-    visited[start[0]][start[1]] = 1
-
+def bfs(a, b) :
+    p = 1
+    queue = deque([(a, b)]) # 시작점
+    visited[a][b] = True
     dx = [1, -1, 0, 0]
     dy = [0, 0, 1, -1]
     
-    while queue:
-        x, y = queue.popleft()
+    while queue : 
+        x, y= queue.popleft()
         
-        for i in range(4):
+        for i in range(4) :
             nx = x + dx[i]
             ny = y + dy[i]
             
-            if nx <0 or nx >=n or ny <0 or ny>=m:
+            if nx <0 or nx >=  n or ny < 0 or ny >= m :
                 continue
             
-            if graph[nx][ny] == 0:
-                continue
-            
-            if visited[nx][ny] == 0 and graph[nx][ny] == 1:
-                visited[nx][ny] = 1
+            if not visited[nx][ny] and paper[nx][ny] == 1 :
+                visited[nx][ny] = True
                 queue.append((nx, ny))
-                size +=1
-    return size
+                p += 1
+    return p
 
-visited = [[0]*m for _ in range(n)]
 cnt = 0
-max_size = 0
-for i in range(n):
-    for j in range(m):
-        if visited[i][j] == 0 and papers[i][j] == 1:
+max_p = 0
+for i in range(n) :
+    for j in range(m) :
+        if paper[i][j] == 1 and not visited[i][j] :
             cnt += 1
-            max_size = max(max_size, bfs((i,j), papers, visited))
-            
+            max_p = max(max_p, bfs(i, j))
 print(cnt)
-print(max_size)
+print(max_p)
