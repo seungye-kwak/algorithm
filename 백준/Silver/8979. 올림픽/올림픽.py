b3552f8b18d06be1972d185d7b416f4a98481bc6
sseung_ye <112370282+seungye-kwak@users.noreply.github.com>
2025-08-29 00:47:53 +0900

    [Silver V] Title: 올림픽, Time: 36 ms, Memory: 32412 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -0,0 +1,22 @@
+n, k = map(int, input().split())
+d = []
+for _ in range(n) :
+    i, g, s, b = map(int, input().split())
+    d.append((i, g, s, b))
+    
+d.sort(key=lambda x: (-x[1], -x[2], -x[3]))
+
+prev_rank = 0
+prev_medal = None
+rank = {}
+for i, (idx, g, s, b) in enumerate(d) :
+    medal = (g, s, b)
+    if i == 0 :
+        prev_rank = 1
+    else :
+        if medal != prev_medal :
+            prev_rank += 1
+    rank[idx] = prev_rank
+    prev_medal = medal
+    
+print(rank[k])
